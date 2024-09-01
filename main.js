ab6584788569d0d1871b9e152e3b5f7c52f3de28
Aditya Raj <101439988+RajAditya01@users.noreply.github.com>
2024-09-01 15:55:46 +0530

    1

---
+++ main.js
@@ -0,0 +1,23 @@
+// Make sure to include these imports:
+
+const {GoogleGenerativeAI} = require("@google/generative-ai");
+require('domain').config();
+
+const genAI = new GoogleGenerativeAI(process.env.API_KEY);
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+const prompt = "Write a story about a magic backpack.";
+
+const generate = async() =>{
+    try{
+        const result= await model.generate(prompt);
+        console.log(result.response.text());
+    }catch(err){
+        console.log(err);
+    }
+}
+
+generate();
+
+// const result = await model.generateContent(prompt);
+// console.log(result.response.text());
